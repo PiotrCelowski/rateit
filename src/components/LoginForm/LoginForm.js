c6ffdd10e094b923a404161ddd9897c8344f6a1b
Anna Pravdiukova <annagekata82@gmail.com>
2024-03-04 19:00:47 +0200

    added react-hook-form validation to sign in form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -14,7 +14,7 @@ import {
 } from "../../api/FirebaseAuthApi";
 import { useDispatch } from "react-redux";
 import { loginActions } from "../../store/loginSlice";
-import { onAuthStateChanged } from "firebase/auth";
+import { AuthErrorCodes, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../configuration/firebase/FirebaseCommon";
 import GoogleIcon from "@mui/icons-material/Google";
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -24,6 +24,8 @@ import MuiAlert from "@mui/material/Alert";
 import { Divider, Stack } from "@mui/material";
 import { PrimaryButton } from "../PrimaryButton/PrimaryButton";
 import { PasswordInput } from "../PasswordInput/PasswordInput";
+import { useForm, Controller } from "react-hook-form";
+import { emailRule } from "../../utils/validateRules";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -32,24 +34,40 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 export default function SignIn() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [errorMessage, setError] = useState([]);
+  const [errorMessage, setErrorMessage] = useState([]);
   const [snackbarOpened, setSnackbarOpened] = useState(false);
 
-  const sumbitHandler = async (event) => {
-    event.preventDefault();
-
-    const data = new FormData(event.currentTarget);
-    signInWithEmail(
-      data.get("email"),
-      data.get("password"),
-      data.get("rememberMe")
-    )
-      .then(() => setAuthCallback())
-      .catch((error) => {
-        setError(error.message);
-        setSnackbarOpened(true);
-      });
-  };
+  const { control, handleSubmit, formState: { errors }, setError, setFocus } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: true,
+    }
+  })
+  const onSubmit = async ({ email, password, rememberMe }) => {
+    try {
+      await signInWithEmail(email, password, rememberMe)
+      setAuthCallback()
+    } catch (error) {
+      // -- handle firebase errors with codes --
+      if (error?.code === AuthErrorCodes.INVALID_PASSWORD) {
+        setFocus('password')
+        setError('password')
+        // -- or set error message directly to the password input field --
+        // setError('password', { message: 'Wrong password' })
+        // return;
+      }
+      if (error?.code === AuthErrorCodes.USER_DELETED) {
+        setFocus('email')
+        setError('email')
+        // -- or set error message directly to the email input field --
+        // setError('email', { message: 'User not found' })
+        // return;
+      }
+      setErrorMessage(error.message);
+      setSnackbarOpened(true);
+    }
+  }
 
   const closeSnackbar = () => {
     setSnackbarOpened(false);
@@ -61,7 +79,7 @@ export default function SignIn() {
       setAuthCallback();
     } catch (error) {
       console.log("signInWithFacebook [error]:", error?.message);
-      setError(error?.message || "Error: Auth with Facebook was not completed");
+      setErrorMessage(error?.message || "Error: Auth with Facebook was not completed");
       setSnackbarOpened(true);
     }
   };
@@ -72,7 +90,7 @@ export default function SignIn() {
       setAuthCallback();
     } catch (error) {
       console.log("signInWithGoogle [error]:", error?.message);
-      setError(error?.message || "Error: Auth with Google was not completed");
+      setErrorMessage(error?.message || "Error: Auth with Google was not completed");
       setSnackbarOpened(true);
     }
   };
@@ -99,29 +117,53 @@ export default function SignIn() {
         <Typography component="h1" variant="h5" textAlign={"center"} mb={1}>
           Sign In
         </Typography>
-        <Stack component="form" onSubmit={sumbitHandler} direction="column" width={"100%"} rowGap={2.5}>
-          <TextField
-            required
-            fullWidth
-            id="email"
-            type='email'
-            label="Email"
+        <Stack component="form" noValidate onSubmit={handleSubmit(onSubmit)} direction="column" width={"100%"} rowGap={2.5}>
+          <Controller
+            control={control}
             name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <PasswordInput
-            required
+            rules={{ required: "Email is required", ...emailRule }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                required
+                fullWidth
+                id="email"
+                type='email'
+                label="Email"
+                autoComplete="email"
+                autoFocus
+                error={Boolean(errors.email)}
+                helperText={errors?.email?.message}
+              />
+          )}/>
+          <Controller
+            control={control}
             name="password"
-            autoComplete="current-password"
+            rules={{ required: "Enter your password" }}
+            render={({ field }) => (
+              <PasswordInput
+                {...field}
+                required
+                name="password"
+                autoComplete="current-password"
+                error={Boolean(errors?.password)}
+                errorMessage={errors?.password?.message}
+              />
+            )}
           />
 
           <Stack direction="row" columnGap={1} alignItems="baseline">
             <Box sx={{ flexGrow: 1 }}>
-              <FormControlLabel
-                control={<Checkbox color="primary" defaultChecked />}
-                name="rememberMe"
-                label="Remember me"
+              <Controller
+                control={control}
+                name='rememberMe'
+                render={({ field }) => (
+                  <FormControlLabel
+                    {...field}
+                    control={<Checkbox color="primary" checked={field?.value} />}
+                    label="Remember me"
+                  />
+                )}
               />
             </Box>
             {/* Todo: uncomment when forgot password feature will be added */}
@@ -148,11 +190,11 @@ export default function SignIn() {
               Or sign in with
             </Typography>
 
-            <IconButton onClick={googleHandler} color="default" size="large">
+            <IconButton type='button' onClick={googleHandler} color="default" size="large">
               <GoogleIcon />
             </IconButton>
 
-            <IconButton onClick={facebookHandler} color="default" size="large">
+            <IconButton type='button' onClick={facebookHandler} color="default" size="large">
               <FacebookIcon />
             </IconButton>
           </Stack>
