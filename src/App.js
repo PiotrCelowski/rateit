512dfb743c88ac7ff0cbd236c771c29add95d5c3
Anna Pravdiukova <annagekata82@gmail.com>
2024-03-11 19:34:14 +0200

    added error page, refactored route loading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter, json } from "react-router-dom";
 import RootLayout from "./utils/RootLayout";
 import { InstantSearch } from 'react-instantsearch-hooks-web';
 import { searchClient } from "./configuration/algolia/algolia-config";
@@ -12,36 +12,8 @@ import EditCoursePage from "./pages/EditCoursePage";
 import UserSettingsPage from "./pages/UserSettingsPage";
 import { ProtectedRoute } from "./pages/ProtectedRoute";
 import { SigninLayout } from "./utils/SigninLayout";
-import { fetchCourse } from "./api/FirestoreApi";
-import { CourseDetaisPage } from "./pages/CourseDetaisPage";
-
-const getCourseDetails = async ({ params }) => {
-  const { courseID } = params
-
-  if (courseID) {
-    try {
-      const response = await fetchCourse(courseID);
-      if (response.exists()) {
-        const courseData = {
-          title: response.data()?.title,
-          author: response.data()?.author,
-          rating: response.data()?.rating,
-          ratingVotes: response.data()?.ratingVotes,
-          codeSnippetsWorking: response.data()?.codeSnippetsWorking,
-          easilyExplained: response.data()?.easilyExplained,
-          keptUpToDate: response.data()?.keptUpToDate,
-          topicCoverage: response.data()?.topicCoverage,
-          organization: response.data()?.organization
-        }
-        return { data: courseData }
-      }
-      throw new Response("Not Found", { status: 404 });
-    } catch (error) {
-      const data = { error: error }
-      return new Response.json(JSON.stringify(data), { status: error?.status || 500})
-    }
-  }
-}
+import { CourseDetaisPage, getCourseDetails } from "./pages/CourseDetaisPage";
+import { ErrorPage } from "./pages/ErrorPage";
 
 function App() {
   const router = createBrowserRouter([
@@ -52,12 +24,13 @@ function App() {
         { path: "/", element: <LandingPage /> },
         { 
           path: '/courses',
+          // Todo: add index route element
           children: [
             {
-              path: "/courses/:courseID",
+              path: ":courseID",
               element: <CourseDetaisPage />,
               loader: getCourseDetails,
-              errorElement: <div>Can't get course</div>
+              errorElement: <ErrorPage />
             }
           ]
         },
--- src/pages/CourseDetaisPage.js
+++ src/pages/CourseDetaisPage.js
@@ -1,5 +1,35 @@
-import { useLoaderData } from "react-router-dom"
+import { json, useLoaderData } from "react-router-dom"
 import Box from "@mui/material/Box"
+import { fetchCourse } from "../api/FirestoreApi";
+
+export const getCourseDetails = async ({ params }) => {
+  const { courseID } = params
+
+  if (courseID) {
+    try {
+      const response = await fetchCourse(courseID);
+      if (response.exists()) {
+        const courseData = {
+          title: response.data()?.title,
+          author: response.data()?.author,
+          rating: response.data()?.rating,
+          ratingVotes: response.data()?.ratingVotes,
+          codeSnippetsWorking: response.data()?.codeSnippetsWorking,
+          easilyExplained: response.data()?.easilyExplained,
+          keptUpToDate: response.data()?.keptUpToDate,
+          topicCoverage: response.data()?.topicCoverage,
+          organization: response.data()?.organization
+        }
+        return { data: courseData }
+      }
+      throw new json('Not Found',
+        { status: 404, statusText: "Course is not exists in DB" },
+      );
+    } catch (error) {
+      throw new json('', { status: error?.status || 500, statusText: error?.statusText})
+    }
+  }
+}
 
 export const CourseDetaisPage = () => {
   const { data } = useLoaderData()
