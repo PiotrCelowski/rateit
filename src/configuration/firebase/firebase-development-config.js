92154e3f6155e206a181c6f017a7086db8a76911
Piotr <piotr.celowski@gmail.com>
2024-03-24 12:15:26 +0100

    Add new config files

--- src/configuration/firebase/firebase-development-config.js
+++ src/configuration/firebase/firebase-development-config.js
@@ -1,9 +1,9 @@
-export const firebaseDevelopmentConfig = {
+export const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "rateit-development.firebaseapp.com",
     projectId: "rateit-development",
     storageBucket: "rateit-development.appspot.com",
     messagingSenderId: "531864487766",
-    appId: "1:531864487766:web:e946bb01386cf268dd38f7",
-    measurementId: "G-4PM2G2BTLK"
-};
+    appId: "1:531864487766:web:284eeb91edbefd5bdd38f7",
+    measurementId: "G-YB5PQ9NWMC"
+  };
